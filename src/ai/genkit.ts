d939012e558d8446c545060dbecb28c824f5dbaf
coiffman tuna <coiffmantuna22@gmail.com>
2025-11-28 19:13:58 +0000

    Try fixing this error: `Console Error: TypeError: Cannot read properties

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,6 +1,12 @@
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/google-genai';
+import {nextPlugin} from '@genkit-ai/next';
 
 export const ai = genkit({
-  plugins: [googleAI()],
+  plugins: [
+    googleAI(),
+    nextPlugin({
+      //
+    }),
+  ],
 });
